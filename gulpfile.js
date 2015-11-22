26d99c01374da4120f7c7471fade23d0f5ba9c10
kenticny <kenticny@gmail.com>
2015-11-22 16:17:59 +0800

    gulp browserify

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+
+gulp.task('browserify', function() {
+
+    var bundle = browserify(['public/javascripts/app.js'], {
+        transform: [reactify],
+        debug: true,
+        fullPaths: true
+    }).bundle();
+
+    return bundle
+          .pipe(source('app.js'))
+          .pipe(gulp.dest('public/javascripts/dist'));
+});
+
+gulp.task('default', ['browserify']);

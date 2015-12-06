0a13dca68124c52f8f29507608ca27ba88e049cf
kenticny <kenticny@gmail.com>
2015-12-07 00:29:00 +0800

    update gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,33 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 
-gulp.task('browserify', function() {
-    var bundle = browserify(['public/javascripts/app.js'], {
+function browserifyBundle(debug) {
+    var opts = {
         transform: [reactify],
-        debug: true,
-        fullPaths: true
-    }).bundle();
+        debug: false,
+        fullPaths: true,
+        cache: {},
+        packageCache: {}
+    };
+    if(debug) {
+        opts.debug = true;
+    }
+    return browserify(['public/javascripts/app.js'], opts);
+}
 
-    return bundle
+var b = watchify(browserifyBundle(true));
+b.on('update', browserifyDevTask);
+b.on('log', console.log);
+
+function browserifyDevTask() {
+    return b.bundle()
           .pipe(source('app.js'))
           .pipe(gulp.dest('public/dist'));
-});
+}
+
+gulp.task('browserify', browserifyDevTask);
 
 gulp.task('development', function() {
 
